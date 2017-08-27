88a0b5b2c5067a62ffe7dbe586a0b40aba097637
Thomas Edwards <tomo12321@hotmail.com>
2017-08-27 21:48:57 +1200

    ADDED MORE COMMENTS

--- src/animations/CreationAnimation.java
+++ src/animations/CreationAnimation.java
@@ -21,10 +21,10 @@ public class CreationAnimation extends JComponent {
 	public static View v;// creation Animation REALLY needs to know about the view, to calculate cross-pane distances.
 
 	private Piece createdPiece;
-	
+
 	private int animationPercent = 0;// how far through the animation it is.
 	private int updatePercent = 5;// amount to update percent by each update method.
-	
+
 	private double updateDX, updateDY;// amount to update x and y by each time update() is called
 	private double currentX, currentY;// current position of animation.
 
@@ -37,23 +37,24 @@ public class CreationAnimation extends JComponent {
 	 * @param createdPiece
 	 */
 	public CreationAnimation(Piece createdPiece) {
-		v.setGlassPane(this);
+		v.setGlassPane(this);// makes the glass pane visible.
 		v.getGlassPane().setVisible(true);
 		this.createdPiece = createdPiece;
+		width = v.getMainBoard().getWidth() / 12;// width and height of 1 board square.
+		height = v.getMainBoard().getHeight() / 12;
+
+		// calculates start and end points
 		if (createdPiece.getPlayerNumber() == 1) {
-			width = v.getMainBoard().getWidth() / 12;
-			height = v.getMainBoard().getHeight() / 12;
-			setStart(createdPiece, true);
-			setEnd(createdPiece, true);
+			getStart(createdPiece, true);
+			getEnd(createdPiece, true);
 		} else {
-			width = v.getMainBoard().getWidth() / 12;
-			height = v.getMainBoard().getHeight() / 12;
-			setStart(createdPiece, false);
-			setEnd(createdPiece, false);
+			getStart(createdPiece, false);
+			getEnd(createdPiece, false);
 		}
+
+		// calcualtes how much each 'update()' should move the animation along.
 		updateDX = ((endX - currentX) * updatePercent / 100);
 		updateDY = ((endY - currentY) * updatePercent / 100);
-
 	}
 
 	/**
@@ -62,7 +63,7 @@ public class CreationAnimation extends JComponent {
 	 * @param p
 	 * @param yellow
 	 */
-	private void setStart(Piece p, boolean yellow) {
+	private void getStart(Piece p, boolean yellow) {
 		if (yellow) {
 			int crossWidth = 4 + ((p.getPieceNumber() - 1) % 5) * v.getyPieces().getWidth() / 5;
 			int crossHeight = 4 + ((int) (p.getPieceNumber() / 5)) * v.getyPieces().getHeight() / 5;
@@ -82,7 +83,7 @@ public class CreationAnimation extends JComponent {
 	 * @param p
 	 * @param yellow
 	 */
-	private void setEnd(Piece p, boolean yellow) {
+	private void getEnd(Piece p, boolean yellow) {
 		if (yellow) {
 			endX = v.getyPieces().getWidth() + v.getMainBoard().getWidth() * 3 / 12 + 10;
 			endY = v.getMainBoard().getHeight() * 3 / 12 + v.getinfoPanel().getHeight() + 10;
